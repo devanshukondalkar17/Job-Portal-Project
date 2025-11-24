3786e6210c99aa75e04119f051206c81ee43d1c1
devanshukondalkar17 <devanshukondalkar17@gmail.com>
2025-11-24 17:42:07 +0530

    Update DBConnection.java

--- DBConnection.java
+++ DBConnection.java
@@ -4,8 +4,8 @@ import java.sql.SQLException;
 
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/FreelanceDB";
-    private static final String USER = "root"; 
-    private static final String PASS = "doraemon"; 
+    private static final String USER = "your_username"; 
+    private static final String PASS = "your_password"; 
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASS);
